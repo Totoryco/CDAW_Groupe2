a5e587f4ee13c3f9c6190a423c604cdcc9eab2ca
antony <antony.barge@gmail.com>
2021-11-22 09:41:19 +0000

    presque fini card template + .env

--- public/catalogue/resources/views/cardTemplate.blade.php
+++ public/catalogue/resources/views/cardTemplate.blade.php
@@ -15,37 +15,42 @@
         @section('cards')
             @parent
             <!-- Item -->
-
             <div class="col mb-5">
-                <div class="card card-format">
-                    <div class="container slide slide1">
-                        <!-- Product image-->
-                    <a href="display.html">
-                        <img class="card-img-top" src="https://fond-ecran-manga.fr/wp-content/uploads/2020/04/Assassination-Classroom-Nagisa-Karma-Korosensei-Poster-by-450x600.png" alt="..." />
-                        </a>
-                        <div class="card-body">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Name of the anime</h5>
-                                <!-- Product description-->
-                                Date - X episodes
-                                <h6>Genres</h6>
-                                <hr>
-                                <h6>X Likes</h6>
+                <div class="card-row">
+                    <div class="card card-format">
+                        <div class="container slide slide1">
+                            <!-- Product image-->
+                        <a href="display.html">
+                            <img class="card-img-top" src="https://fond-ecran-manga.fr/wp-content/uploads/2020/04/Assassination-Classroom-Nagisa-Karma-Korosensei-Poster-by-450x600.png" alt="..." />
+                            </a>
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Name of the anime</h5>
+                                    <!-- Product description-->
+                                    Date - X episodes
+                                    <h6>Genres</h6>
+                                    <hr>
+                                    <h6>X Likes</h6>
+                                </div>
                             </div>
                         </div>
+                        <!-- Bottom Button-->
+                        <div class="card-footer bg-dark slide slide2">
+                            <a class="btn mt-auto" href="#3">
+                                <i class="fa fa-thumbs-up icon-card-format">
+                            </i></a>
+                            <a class="btn mt-auto" href="#3">
+                                <i class="fa fa-thumbs-down icon-card-format">
+                            </i></a>
+                            <a class="btn mt-auto" href="#3">
+                                <i class="fa fa-list-ul icon-card-format">
+                            </i></a>
+                        </div>
                     </div>
-                    <!-- Bottom Button-->
-                    <div class="card-footer bg-dark slide slide2">
-                        <a class="btn mt-auto" href="#3">
-                            <i class="fa fa-thumbs-up icon-card-format">
-                        </i></a>
-                        <a class="btn mt-auto" href="#3">
-                            <i class="fa fa-thumbs-down icon-card-format">
-                        </i></a>
-                        <a class="btn mt-auto" href="#3">
-                            <i class="fa fa-list-ul icon-card-format">
-                        </i></a>
+                    <!-- Side Text on hover-->
+                    <div class="slide slide3">
+                        <p class="text-white">Je teste un affichage</p>
                     </div>
                 </div>
             </div>
