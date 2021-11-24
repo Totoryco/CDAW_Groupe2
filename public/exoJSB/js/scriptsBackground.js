0b0cd685c509709719e3a7da7c7d11c07f6ea2da
BruceHersemeule <bruce.hersemeule@etu.imt-lille-douai.fr>
2021-11-24 08:07:11 +0000

    Commit exos JS Bruce

--- public/exoJSB/js/scriptsBackground.js
+++ public/exoJSB/js/scriptsBackground.js
@@ -1,3 +1,16 @@
-//document.body.style.backgroundColor = "red";
+"use strict";
 
-document.getElementsByClassName('descr').style.backgroundColor =  "lightblue";
+document.body.style.backgroundColor = "red";
+
+var obj = document.getElementsByClassName("descr");
+
+for (var i = 0; i < obj.length; i++) {
+    obj[i].style.backgroundColor =  "lightblue";
+}
+
+var monCaroussel = document.getElementById("caroussel");
+var plist = monCaroussel.getElementsByTagName("p");
+plist[0].style.backgroundColor =  "white";
+
+
+
