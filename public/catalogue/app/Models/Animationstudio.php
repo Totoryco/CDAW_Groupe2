1f606d76491f6ff46ecaf5a4a586709a84b31d5e
Antony <antony.barge@gmail.com>
2021-12-01 10:39:59 +0000

    sans maj

--- public/catalogue/app/Models/Animationstudio.php
+++ public/catalogue/app/Models/Animationstudio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AnimationStudio extends Model
+class Animationstudio extends Model
 {
     use HasFactory;
 
